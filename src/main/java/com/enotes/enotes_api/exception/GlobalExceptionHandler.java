3f29ec54f3f3593ef0212ef6f4a8ea1526334276
040govind <govinddangi7806@gmail.com>
2025-01-05 16:04:47 +0530

    custom validation complete

--- src/main/java/com/enotes/enotes_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/enotes_api/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.enotes.enotes_api.exception;
+import java.util.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,6 +31,23 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleResourceNotFoundException(Exception e) {
 		log.error("GlobalExceptionHandler :: handleResourceNotFoundException ::", e.getMessage());
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	// @ExceptionHandler(MethodArgumentNotValidException.class)
+	// public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+	// 	//log.error("GlobalExceptionHandler :: handleException ::", e.getMessage());
+	// 	   List<ObjectError> allError= e.getBindingResult().getAllErrors();
+	// 	   Map<String,Object> error = new LinkedHashMap<>();
+	// 	   allError.stream().forEach(er->{
+	// 		 String msg = er.getDefaultMessage();
+	// 		 String field = ((FieldError)(er)).getField();
+	// 		 error.put(field,msg);
+	// 	   });
+	// 	return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	// }
+
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<?> handleValidationException(ValidationException e) {
+		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
 	}
 
 }
